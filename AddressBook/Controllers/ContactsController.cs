a2451d1ca627412442c99dee179d95c57a09f079
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-10 23:36:30 +0600

    Contact Model

---
+++ AddressBook/Controllers/ContactsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AddressBook.Models;
+
+namespace AddressBook.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ContactsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Contacts / Index
+
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ AddressBook/Migrations/201812101727338_InitialModel.cs
@@ -0,0 +1,30 @@
+namespace AddressBook.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FullName = c.String(nullable: false, maxLength: 255),
+                        NickName = c.String(),
+                        Address = c.String(),
+                        Email = c.String(),
+                        Birthdate = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Contacts");
+        }
+    }
+}
